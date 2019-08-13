1e16abc385ef29d4561af67918c62935331a8f72
Eric Mignot <eric.mignot@gmail.com>
2019-08-13 08:40:29 -0700

    xml formatting handles more cases

--- docs/2.1.cleanify.xml.spec.js
+++ docs/2.1.cleanify.xml.spec.js
@@ -32,4 +32,22 @@ describe('cleanify xml', ()=> {
 
         expect(actual).to.equal(`<hello>\n    <world/>\n</hello>\n`)
     })
+
+    it('works with one nested child', ()=>{
+        var actual = cleanifyXml('<greetings><hello>world</hello></greetings>')
+
+        expect(actual).to.equal(`<greetings>\n    <hello>\n        world\n    </hello>\n</greetings>\n`)
+    })
+
+    it('works with two nested children', ()=>{
+        var actual = cleanifyXml('<greetings><hello>world</hello><welcome>here</welcome></greetings>')
+
+        expect(actual).to.equal(`<greetings>\n    <hello>\n        world\n    </hello>\n    <welcome>\n        here\n    </welcome>\n</greetings>\n`)
+    })
+
+    it('works with two children of different nature', ()=>{
+        var actual = cleanifyXml('<greetings><hello value="world"/><welcome>here</welcome></greetings>')
+
+        expect(actual).to.equal(`<greetings>\n    <hello value="world"/>\n    <welcome>\n        here\n    </welcome>\n</greetings>\n`)
+    })
 })
